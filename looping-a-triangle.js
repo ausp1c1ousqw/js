55e4aebfe62e843455e47fddcc38ec68ff8ac2f8
Cherniavskiy Sergey <qausp1c1ousw@gmail.com>
2023-12-19 15:45:48 +0300

    looping a triangle

---
+++ looping-a-triangle.js
@@ -0,0 +1,8 @@
+// let outputString = '#'
+//  }
+// while (outputString.length < 8) {
+//     console.log(`${outputString}`)
+//     outputString += '#'
+// }
+for (let line = '#'; line.length < 8; line += '#')
+    console.log(line)
