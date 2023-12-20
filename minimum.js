3018c9ba5d009cf2e5385bfc66ceaadfe9b49870
Cherniavskiy Sergey <qausp1c1ousw@gmail.com>
2023-12-20 16:41:40 +0300

    minimum function

---
+++ minimum.js
@@ -0,0 +1,8 @@
+function min(firstNumber, secondNumber){
+    if(firstNumber > secondNumber)
+        return secondNumber
+    else if(firstNumber < secondNumber)
+        return firstNumber
+    else 
+        return 'Numbers are equal'
+}
