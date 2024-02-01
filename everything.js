89c56114d925a9a6bbcb650dfccefbf916f4c411
Cherniavskiy Sergey <qausp1c1ousw@gmail.com>
2024-02-01 12:24:02 +0300

    everything

---
+++ everything.js
@@ -0,0 +1,14 @@
+// function every(array, testFunc) {
+//   for(el of array) {
+//     if(!testFunc(el))
+//       return false 
+//   }
+//   return true
+// }
+
+function every(array, testFunc) {
+ return !array.some(el => !testFunc(el))
+}
+
+console.log(every([1, 3, 5], n => n < 10))
+console.log(every([2, 4, 16], n => n < 10))
